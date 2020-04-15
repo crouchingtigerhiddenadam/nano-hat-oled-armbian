957d8c53a550f9670475dbf199dc88ff8f8638c9
crouchingtigerhiddenadam <12783348+crouchingtigerhiddenadam@users.noreply.github.com>
2020-04-15 21:49:07 +0100

    release candidate

--- oled-start.py
+++ oled-start.py
@@ -31,9 +31,11 @@ import smbus
 import subprocess
 import time
 
-SCREEN_BLANK_TIMEOUT = 30
+DISPLAY_OFF_TIMEOUT = 30
 
 current_time = time.time()
+display_refresh_time = 0
+display_off_time = current_time + DISPLAY_OFF_TIMEOUT
 i2c0_bus = smbus.SMBus(0) # access to OLED
 image = Image.new('1', (128, 64))
 image_draw = ImageDraw.Draw(image)
@@ -41,12 +43,10 @@ image_font8 = ImageFont.truetype('DejaVuSansMono.ttf', 8)
 image_font10 = ImageFont.truetype('DejaVuSansMono.ttf', 10)
 image_font15 = ImageFont.truetype('DejaVuSansMono.ttf', 15)
 image_font25 = ImageFont.truetype('DejaVuSansMono.ttf', 25)
-key1_page_index = 1
-key2_page_index = 2
-key3_page_index = 3
-page_index = 0
-page_refresh_time = 0
-screen_blank_time = current_time + SCREEN_BLANK_TIMEOUT
+key1_cmd_index = 1
+key2_cmd_index = 2
+key3_cmd_index = 3
+cmd_index = 0
 shutdown_time = 0
 
 def write_i2c_image_data(i2c_bus, image):
@@ -80,7 +80,7 @@ try:
   with open('/sys/class/gpio/gpio3/direction', 'w') as file: file.write('in\n')
 
   i2c0_bus.write_i2c_block_data(0x3c, 0x00, [
-    0xae,       # set display off 
+    0xae,       # set display off
     0x00,       # set lower column address
     0x10,       # set higher column address
     0x40,       # set display start line
@@ -106,40 +106,40 @@ try:
     current_time = time.time()
     with open('/sys/class/gpio/gpio0/value') as file: # poll key1 down
       if file.read(1) == '1':
-        page_index = key1_page_index
-        page_refresh_time = 0
-        screen_blank_time = current_time + SCREEN_BLANK_TIMEOUT
+        cmd_index = key1_cmd_index
+        display_refresh_time = 0
+        display_off_time = current_time + DISPLAY_OFF_TIMEOUT
         continue
     with open('/sys/class/gpio/gpio2/value') as file: # poll key2 down
       if file.read(1) == '1':
-        page_index = key2_page_index
-        page_refresh_time = 0
-        screen_blank_time = current_time + SCREEN_BLANK_TIMEOUT
+        cmd_index = key2_cmd_index
+        display_refresh_time = 0
+        display_off_time = current_time + DISPLAY_OFF_TIMEOUT
         continue
     with open('/sys/class/gpio/gpio3/value') as file: # poll key3 down
       if file.read(1) == '1':
-        page_index = key3_page_index
-        page_refresh_time = 0
-        screen_blank_time = current_time + SCREEN_BLANK_TIMEOUT
+        cmd_index = key3_cmd_index
+        display_refresh_time = 0
+        display_off_time = current_time + DISPLAY_OFF_TIMEOUT
         continue
-    if current_time > screen_blank_time:
+    if current_time > display_off_time:
       i2c0_bus.write_i2c_block_data(0x3c, 0x00, [0xae]) # set display off
       continue
-    elif current_time > page_refresh_time:
+    elif current_time > display_refresh_time:
       i2c0_bus.write_i2c_block_data(0x3c, 0x00, [0xaf]) # set display on
-      if page_index == -1: # break and shutdown page_index -1
-        break
-      elif page_index == 0:
+      if cmd_index == 0:
+        key1_cmd_index = 1
+        key2_cmd_index = 2
+        key3_cmd_index = 3
         splash = Image.open('splash.png')
         image.paste(splash)
         write_i2c_image_data(i2c0_bus, image)
         splash.close()
-	page_index = 1 # redirect to page 1
-        page_refresh_time = current_time + SCREEN_BLANK_TIMEOUT
-      elif page_index == 1:
-	key1_page_index = 1
-	key2_page_index = 2
-	key3_page_index = 3
+        display_refresh_time = current_time + DISPLAY_OFF_TIMEOUT
+      elif cmd_index == 1:
+	key1_cmd_index = 0
+	key2_cmd_index = 2
+	key3_cmd_index = 3
         text1 = time.strftime('%A')
         text2 = time.strftime('%e %b %Y')
         text3 = time.strftime('%X')
@@ -147,14 +147,14 @@ try:
         image_draw.text((6,  2), text1, 1, image_font15)
         image_draw.text((6, 20), text2, 1, image_font15)
         image_draw.text((6, 36), text3, 1, image_font25)
-        page_refresh_time = current_time + 1
+        display_refresh_time = current_time + 1
         write_i2c_image_data(i2c0_bus, image)
-      elif page_index == 2:
-	key1_page_index = 1
-	key2_page_index = 2
-	key3_page_index = 3
+      elif cmd_index == 2:
+	key1_cmd_index = 1
+	key2_cmd_index = 0
+	key3_cmd_index = 3
         text1 = subprocess.check_output("ip a show | grep -E '^\s*inet' | grep -m1 global | awk '{printf \"IPv4: %s\", $2}' | sed 's|/.*||'", shell = True)
-        text2 = subprocess.check_output("df -h | awk '$NF==\"/\"{printf \"HDD:  %d/%dGB %s\", $3,$2,$5}'", shell = True)
+        text2 = subprocess.check_output("df -h | awk '$NF==\"/\"{printf \"card: %d/%dGB %s\", $3,$2,$5}'", shell = True)
         text3 = subprocess.check_output("free -m | awk 'NR==2{printf \"RAM:  %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'", shell = True)
         text4 = subprocess.check_output("top -bn1 | grep load | awk '{printf \"Load: %.2f\", $(NF-2)}'", shell = True)
         text5 = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"Temp: %3.1fc\", $1/1000}'", shell = True)
@@ -164,32 +164,34 @@ try:
         image_draw.text((6, 26), text3, 1, image_font10)
         image_draw.text((6, 38), text4, 1, image_font10)
         image_draw.text((6, 50), text5, 1, image_font10)
-        page_refresh_time = current_time + 2
+        display_refresh_time = current_time + 2
         write_i2c_image_data(i2c0_bus, image)
-      elif page_index == 3:
-	key1_page_index = 1
-	key2_page_index = 2
-	key3_page_index = 4
+      elif cmd_index == 3:
+	key1_cmd_index = 1
+	key2_cmd_index = 2
+	key3_cmd_index = 4
         image_draw.rectangle((0, 0, 128, 64), 0)
         image_draw.text((6, 2), 'Shutdown?', 1, image_font15)
         image_draw.rectangle((4, 22, 124, 34), 1)
         image_draw.text((6, 22), 'No',  0, image_font10)
         image_draw.text((6, 36), 'Yes', 1, image_font10)
         image_draw.text((6, 54), 'F3: Toggle Choices', 1, image_font8)
-        page_refresh_time = current_time + 5
+        display_refresh_time = current_time + 5
         write_i2c_image_data(i2c0_bus, image)
-      elif page_index == 4:
-	key1_page_index = -1
-	key2_page_index = 4
-	key3_page_index = 3
+      elif cmd_index == 4:
+	key1_cmd_index = 99
+	key2_cmd_index = 4
+	key3_cmd_index = 3
         image_draw.rectangle((0, 0, 128, 64), 0)
         image_draw.text((6, 2), 'Shutdown?', 1, image_font15)
         image_draw.rectangle((4, 36, 124, 48), 1)
         image_draw.text((6, 22), 'No',  1, image_font10)
         image_draw.text((6, 36), 'Yes', 0, image_font10)
         image_draw.text((6, 54), 'F1: Confirm Shutdown', 1, image_font8)
-        page_refresh_time = current_time + 5
+        display_refresh_time = current_time + 5
         write_i2c_image_data(i2c0_bus, image)
+      elif cmd_index == 99: # break and shutdown cmd_index 99
+        break
 
 except KeyboardInterrupt:
   print(' CTRL+C detected')
@@ -201,7 +203,7 @@ finally:
   with open('/sys/class/gpio/unexport', 'w') as file: file.write('2\n') # release GPIO 2 (key2)
   with open('/sys/class/gpio/unexport', 'w') as file: file.write('3\n') # release GPIO 3 (key3)
 
-  if page_index == -1: # shutdown now if the page_index was -1
+  if cmd_index == 99: # shutdown now if the  was 99
     os.system('shutdown now')
   else:
     exit(0)
